b2ea7714d64e1d6a467a2fe34bf510d0353fa25c
ihsan <al.aqib@bjitgroup.com>
2023-08-14 13:31:43 +0600

    complete

--- app/src/main/java/com/ihsan/android_jetpack_authentication/MainActivity.kt
+++ app/src/main/java/com/ihsan/android_jetpack_authentication/MainActivity.kt
@@ -1,36 +1,48 @@
 package com.ihsan.android_jetpack_authentication
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImagePainter.State.Empty.painter
+import coil.compose.AsyncImage
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.ihsan.android_jetpack_authentication.ui.theme.Android_jetpack_authenticationTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,6 +52,27 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var mAuth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    private val resultActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        // result returned from launching the intent from GoogleSignInApi.getSignInIntent()
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            val exception = task.exception
+            if (task.isSuccessful) {
+                try {
+                    // Google SignIn was successful, authenticate with Firebase
+                    val account = task.getResult(ApiException::class.java)!!
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: Exception) {
+                    // Google SignIn Failed
+                    Log.d("SignIn", "Google SignIn Failed")
+                }
+            } else {
+                Log.d("SignIn", exception.toString())
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,6 +108,68 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        resultActivityResultLauncher.launch(signInIntent)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // SignIn Successful
+                    Toast.makeText(this, "SignIn Successful", Toast.LENGTH_SHORT).show()
+                    setContent {
+                        Android_jetpack_authenticationTheme {
+                            val user: FirebaseUser = mAuth.currentUser!!
+                            ProfileScreen(
+                                profileImage = user.photoUrl!!,
+                                name = user.displayName!!,
+                                email = user.email!!,
+                                signOutClicked = {
+                                    signOut()
+                                }
+                            )
+                        }
+                    }
+                } else {
+                    // SignIn Failed
+                    Toast.makeText(this, "SignIn Failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun signOut() {
+        // get the google account
+        val googleSignInClient: GoogleSignInClient
+
+        // configure Google SignIn
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Sign Out of all accounts
+        mAuth.signOut()
+        googleSignInClient.signOut()
+            .addOnSuccessListener {
+                Toast.makeText(this, "Sign Out Successful", Toast.LENGTH_SHORT).show()
+                setContent {
+                    Android_jetpack_authenticationTheme {
+                        GoogleSignInButton {
+                            signIn()
+                        }
+                    }
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Sign Out Failed", Toast.LENGTH_SHORT).show()
+            }
     }
 }
 
@@ -116,17 +211,73 @@ fun GoogleSignInButton(signInClicked: () -> Unit) {
                         .padding(start = 20.dp)
                         .align(Alignment.CenterVertically),
                     text = "Sign In With Google",
-                    fontSize = MaterialTheme.typography.h6.fontSize,
+                    fontSize = MaterialTheme.typography.bodyMedium.fontSize,
                 )
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    Android_jetpack_authenticationTheme {
-        GoogleSignInButton(signInClicked = {})
+fun ProfileScreen(
+    profileImage: Uri,
+    name: String,
+    email: String,
+    signOutClicked: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
+            modifier = Modifier
+                .size(150.dp)
+                .fillMaxHeight(0.4f),
+            shape = RoundedCornerShape(125.dp),
+        ) {
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                model = profileImage,
+                contentDescription = "profile_photo",
+                contentScale = ContentScale.FillBounds
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxHeight(0.6f)
+                .padding(top = 60.dp)
+        ) {
+            OutlinedTextField(
+                value = name,
+                onValueChange = {},
+                readOnly = true,
+                label = {
+                    Text(text = "Name")
+                },
+            )
+
+            OutlinedTextField(
+                modifier = Modifier.padding(top = 20.dp),
+                value = email,
+                onValueChange = {},
+                readOnly = true,
+                label = {
+                    Text(text = "Email")
+                },
+            )
+
+            Button(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 100.dp),
+                onClick = { signOutClicked() }
+            ) {
+                Text(text = "LogOut")
+            }
+        }
+
     }
 }
